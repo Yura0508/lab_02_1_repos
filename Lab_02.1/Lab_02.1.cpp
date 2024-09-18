f387c61639e44803604f37c97814d86fe39860ad
Yura <urijkisil2007Qgmail.com>
2024-09-18 23:54:47 +0300

    first master commit

---
+++ Lab_02.1/Lab_02.1.cpp
@@ -0,0 +1,27 @@
+﻿// Lab_02.cpp
+// < Кісіль Юрій >
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 7
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double z1;
+	double z2;
+	double a;
+	double b;
+	double PI = 4 * atan(1.0);
+
+	cout << "a = "; cin >> a;
+
+	z1 = pow(cos(PI * 3 / 8 - a / 4), 2) - pow(cos((PI * 11 / 8) + a / 4), 2);
+	z2 = sqrt(2) / 2 * sin(a / 2);
+
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+
+	return 0;
+}
